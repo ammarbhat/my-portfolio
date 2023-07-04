e11bcda34f1ded505186879c4ce12ef907e80bcf
Ammar <bhatmmr@gmail.com>
2023-07-04 22:05:26 +0530

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./Components/Header"
 import About from "./Components/About"
-import Projects from "./Components/projects"
+import Projects from "../Components/Projects"
 import Contact from "./Components/Contact"
 import { Element } from 'react-scroll'
 import Footer from "./Components/Footer"

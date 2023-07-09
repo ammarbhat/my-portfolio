bbe9c570ecac9eacdac0628b13fcff4445740e44
Ammar <bhatmmr@gmail.com>
2023-07-10 00:25:36 +0530

    added second project

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { Link } from "react-scroll"
 function Header() {
-  
+
   return (
-     
-        <nav className='bg-white h-20 shadow-xl w-full flex justify-end z-4'>
-            <ul className='flex m-7 items-center'>
-            <Link to="about" smooth={true} duration={500}><li className='m-3 text-xl text-gray-700'>About</li></Link>
-            <Link to="projects" smooth={true} duration={500}><li className='m-3 text-xl text-gray-700'>Projects</li></Link>
-            <Link to="contact" smooth={true} duration={500}><li className='m-3 text-xl text-gray-700'>Contact</li></Link>
-                
-            </ul>
-        
-        </nav>
-   
+
+    <nav className='bg-white h-20 shadow-xl w-full flex justify-end z-4'>
+      <ul className='flex m-7 items-center'>
+        <Link to="about" smooth={true} duration={500}><li className='m-3 text-xl text-gray-700'>About</li></Link>
+        <Link to="projects" smooth={true} duration={500}><li className='m-3 text-xl text-gray-700'>Projects</li></Link>
+        <Link to="contact" smooth={true} duration={500}><li className='m-3 text-xl text-gray-700'>Contact</li></Link>
+
+      </ul>
+
+    </nav>
+
   )
 }
 
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -25,6 +25,29 @@ function Projects() {
         
        </div>
    </motion.div>
+
+   </a>
+
+
+
+   <a href="https://dictionary-byammar.netlify.app/" target='blank_' className='overflow-hidden'
+  
+   >
+   <motion.div className='border-2 rounded-xl sm:w-96 w-72 p-4 sm:mx-20 my-20'
+   style={{
+    height: "320px"
+   }}
+   initial={{scale: 1}}
+   whileHover={{scale: 1.1}}
+   >
+       <img src="./dictss.png" className=' rounded-xl ' />
+       <div className='text-gray-600 text-xl'>A simple Dictionary</div>
+       <div className='flex justify-center'>
+       <img src="./react.png" className='w-18 h-16 p-3' />
+       <img src="./tailwind.png" className='w-18 h-16 p-3' />
+        
+       </div>
+   </motion.div>
    </a>
 
    
